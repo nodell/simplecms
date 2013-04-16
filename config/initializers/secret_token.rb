9dfd224d295256e581c3e9727002aa44a913d879
Neil Odell <nodell@me.com>
2013-04-16 12:18:39 -0400

    Initial add GIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = 'f9535734222c89db3b3c1338e390c780b1e4841252e1aa26cf6b19d67b5b771056e51045f98f111e42b321d46e2d7ea58189d5412d2e2729ede2c333943991ca'
