de1a1f17874695b06a93b23f40ec2a56d4d9dd90
Neil Odell <nodell@me.com>
2013-05-24 15:27:11 -0400

    Simple CMS progress

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,26 +1,34 @@
 class DemoController < ApplicationController
+  # by default, controller will render the view with the same name as the action
+  
+  
   def index
 	# redirect actually does a 302 
 	redirect_to(:action => "hello")
   end
   
   def hello
+	@personId = "";
 	# can also redirect to outside URLs
 	# redirect_to("http://www.dartmouth.edu")
+	# or to another action
 	# redirect_to(:action => 'other_hello');
+	# or to another controller & action
 	# redirect_to(:controller => 'newController', :action => 'myaction')
 	
-	# instance variables begin with @, and are automatically available in any of the views for this controller
+	# instance variables begin with @, and are automatically available in the associated view or the one that is rendered
 	@array = [1,2,3,4,5]
-	# instance variable even available in another template!!
-	# render('anotherTemplate'); 
+	# instance variable even available in another view as long as it's called from this action
+	# render('anotherTemplate');
+	@personId = params['pid'];
   end
   
   def other_hello
-	# render just text, not a template
+	# render just text, not a view
 	# render(:text => "Hello Everybody!")
-	# render a different template
+	# render a different view
 	# render(:template => 'anotherTemplate')
+	@array = [1,2,3,4,5]
 	render('anotherTemplate')
 	
   end
